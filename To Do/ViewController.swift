5582c7416f53e0ad58fb0cdef36cdc5c192ebca0
Dima1567 <dima.sava1567@gmail.com>
2021-12-05 17:51:31 +0300

    Initial Commit

---
+++ To Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do
+//
+//  Created by Dima Savitskiy on 5.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
